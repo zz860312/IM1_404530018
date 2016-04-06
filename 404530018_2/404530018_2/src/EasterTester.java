988f685df54b02e1ba7a08053f425892c9b26372
HanselHu <apologize0616@gmail.com>
2016-04-06 18:05:51 +0800

    作業批改完成!

---
+++ 404530018_2/404530018_2/src/EasterTester.java
@@ -0,0 +1,16 @@
+/*
+ * [B]81
+ * [TA's Advise]
+ * 1.請依照題目要求規格建立函式, 此部分扣2分.
+ * 2.實作的演算法錯誤(題目要求為Gauss, 此演算法為Anonymous Gregorian, 等級從B開始計算.
+ * 3.常見的數學method可以記一下: Math.floor(), Math.abs(), Math.round(), Math.sqrt(), and so on..
+ * 4. 之後可以嘗試在javadoc中寫一些註解.
+ * */
+
+public class EasterTester {
+
+	public static void main(String[] args) {
+		System.out.println(Easter.getEasterDate(2001));
+		System.out.println(Easter.getEasterDate(2012));
+	}
+}
